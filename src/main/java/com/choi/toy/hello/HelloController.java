5767ad2ec4a8d4c75528f4f93250356b23a05301
chlwlgus4 <chlwlgus89@naver.com>
2021-11-29 23:42:07 +0900

    웹훅 테스트

--- src/main/java/com/choi/toy/hello/HelloController.java
+++ src/main/java/com/choi/toy/hello/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping(value = {"/", "/hello"})
+    @GetMapping("/hello")
     public String hello() {
         return "helloWorld";
     }
